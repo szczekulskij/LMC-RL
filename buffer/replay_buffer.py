f07c7757271fc99036ddf6b28a7237c07fcacccd
szczekulskij <szczekulskij@gmail.com>
2025-04-21 16:23:34 +0200

    add ParallelReplayBuffer

--- buffer/replay_buffer.py
+++ buffer/replay_buffer.py
@@ -1,10 +1,39 @@
 import collections
 import numpy as np
 import torch
+from abc import ABC, abstractmethod
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-class ReplayBuffer:
+class ReplayBufferSuperClass(ABC):
+    """Abstract base class for replay buffers."""
+    @abstractmethod
+    def add(self, *args, **kwargs):
+        """
+        Add a transition to the buffer.
+
+        For basic (eg. non-parallel) ReplayBuffer this will take as input state, action, reward, next_state, done
+        For ParallelReplayBuffer this will take same inputs as above, but in the form of a batch of transitions each
+
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def sample(self, batch_size: int):
+        """Sample a batch of transitions."""
+        raise NotImplementedError
+
+    @abstractmethod
+    def __len__(self):
+        """Return the current size of the replay buffer."""
+        raise NotImplementedError
+    
+    def set_seed(self, seed: int):
+        """Set the random seed for reproducibility."""
+        np.random.seed(seed)
+        torch.manual_seed(seed)
+
+class ReplayBuffer(ReplayBufferSuperClass):
     '''
     Pre-allocate the memory for efficiency.
     Store as numpy arrays and convert to tensors when sampling for efficiency as well.
@@ -49,6 +78,86 @@ class ReplayBuffer:
         next_state_batch = torch.tensor(self.next_states[batch_indices], dtype=torch.float32).to(device)
         done_batch = torch.tensor(self.dones[batch_indices], dtype=torch.float32).unsqueeze(1).to(device)
         return state_batch, action_batch, reward_batch, next_state_batch, done_batch
+
+    def __len__(self):
+        """Return the current size of the replay buffer."""
+        return self.size
+    
+    def set_seed(self, seed: int):
+        #TODO: Remove set_seed from both replay buffer implementations since it's implemented in the abstarct class
+        """Set the random seed for reproducibility."""
+        np.random.seed(seed)
+        torch.manual_seed(seed)
+
+class ParallelReplayBuffer(ReplayBufferSuperClass):
+    """Replay buffer for parallel environments."""
+    def __init__(self, state_dim: int, action_dim: int, num_envs: int, capacity: int, device: torch.device):
+        self.capacity = capacity
+        self.num_envs = num_envs
+        self.device = device
+
+        self.per_env_capacity = capacity // num_envs
+        print(f"Per env buffer size is: {self.per_env_capacity} given buffer size {capacity} and num_envs {num_envs}")
+
+        self.states = torch.zeros((self.per_env_capacity, num_envs, state_dim), device=device)
+        self.next_states = torch.zeros((self.per_env_capacity, num_envs, state_dim), device=device)
+        self.actions = torch.zeros((self.per_env_capacity, num_envs, action_dim), device=device)
+        self.rewards = torch.zeros((self.per_env_capacity, num_envs), device=device)
+        self.dones = torch.zeros((self.per_env_capacity, num_envs), device=device)
+        self.ptr = 0
+        self.size = 0  # Initialize size to track the number of transitions
+
+    def add(self, states, next_states, actions, rewards, dones):
+        """Add transitions for parallel environments."""
+        # check that all inputs are of the same shape eg. envs x state_dim
+        for arr in [states, next_states, actions, rewards, dones]:
+            assert arr.shape[0] == self.num_envs, f"Expected {self.num_envs} environments, but got {arr.shape[0]}."
+
+        assert states.shape[1] == self.states.shape[2], f"Expected state_dim {self.states.shape[2]}, but got {states.shape[1]}."
+        assert next_states.shape[1] == self.next_states.shape[2], f"Expected state_dim {self.next_states.shape[2]}, but got {next_states.shape[1]}."
+        assert actions.shape[1] == self.actions.shape[2], f"Expected action_dim {self.actions.shape[2]}, but got {actions.shape[1]}."
+        assert rewards.shape[1] == self.rewards.shape[1], f"Expected num_envs {self.rewards.shape[1]}, but got {rewards.shape[1]}."
+        assert dones.shape[1] == self.dones.shape[1], f"Expected num_envs {self.dones.shape[1]}, but got {dones.shape[1]}."
+
+
+        idx = self.ptr
+        self.states[idx] = states
+        self.next_states[idx] = next_states
+        self.actions[idx] = actions
+        self.rewards[idx] = rewards
+        self.dones[idx] = dones
+
+        
+        self.ptr = (self.ptr + 1) % self.per_env_capacity
+        self.size = min(self.size + self.num_envs, self.per_env_capacity)
+
+    def sample(self, batch_size: int):
+        """Sample a batch of transitions."""
+        batch_indices = np.random.choice(self.size, size=batch_size, replace=False)
+        env_indices = torch.randint(0, self.num_envs, (batch_size,))
+        return (
+            self.states[batch_indices, env_indices],
+            self.next_states[batch_indices, env_indices],
+            self.actions[batch_indices, env_indices],
+            self.rewards[batch_indices, env_indices],
+            self.dones[batch_indices, env_indices],
+        )
+    
+    def sample_flattened(self, batch_size: int):
+        """Sample a batch of transitions and flatten the batch dimension."""
+        batch_indices = np.random.choice(self.size, size=batch_size, replace=False)
+        env_indices = torch.randint(0, self.num_envs, (batch_size,))
+
+        #TODO: Is this the right way to flatten the batch dimension?
+        # We need pairs (state,next_state) to be next to each other in the flattened array
+        # I guess that's achieved by default
+        return (
+            self.states[batch_indices, env_indices].view(-1),
+            self.next_states[batch_indices, env_indices].view(-1),
+            self.actions[batch_indices, env_indices].view(-1),
+            self.rewards[batch_indices, env_indices].view(-1),
+            self.dones[batch_indices, env_indices].view(-1),
+        )
 
     def __len__(self):
         """Return the current size of the replay buffer."""
